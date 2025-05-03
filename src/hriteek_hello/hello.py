9d04237a1139075ac2085544baed306939ef5f64
hritxx <hriteekroy1869@gmail.com>
2025-05-03 18:31:46 +0530

    ex-basics-3

--- src/hriteek_hello/hello.py
+++ src/hriteek_hello/hello.py
@@ -1,4 +1,7 @@
+from typing import Optional
 from rich import print
 
 def say_hello(name: Optional[str] = None) -> None:
     print(f"[bold green]Hello, {name or 'World'}![/bold green]")
+    
+    
